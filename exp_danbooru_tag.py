99c91f74b1af1144b862f38ff434ff19d4332f14
Noksek2 <ileum37@gmail.com>
2025-09-26 06:16:50 +0900

    Update exp_danbooru_tag.py

--- exp_danbooru_tag.py
+++ exp_danbooru_tag.py
@@ -1,7 +1,11 @@
-input_text='''<paste danbooru tag>'''
-#example
-'''? babo 13M
-? merong 15M
-...
-'''
-prompt=','.join([' '.join(tag.split(' ')[:-1]) for tag in input_text.split('\n')]
+def show_cleantag(tag,add_tag={'shiny skin','masterpiece','doujin'},exc_tag={'monochrome','grayscale','greyscale','simple background', 'white background'}):
+  tag2=[line for line in tag.strip().split('\n')][1::2]
+  taglist=[' '.join(t.split(' ')[:-1]) for t in tag2]
+  tag_out=[t for t in taglist if t not in exc_tag]
+  tag_out=(set(tag_out).union(add_tag))
+  print(', '.join(tag_out))
+#
+show_cleantag("""?
+1girl 3k
+?
+school uniform 15k""")
